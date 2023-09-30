23808c05c9e370e8b1620e85a5840b9ed80d5b7f
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-09-30 16:38:59 +0200

    Initial Commit

---
+++ NormandieSpots/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NormandieSpots
+//
+//  Created by Dorian Emenir on 30/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NormandieSpots/NormandieSpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NormandieSpotsApp.swift
+//  NormandieSpots
+//
+//  Created by Dorian Emenir on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct NormandieSpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
