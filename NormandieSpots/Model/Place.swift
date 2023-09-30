5525740e252754db4f3b30e4675ef138d236bae5
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-09-30 18:35:59 +0200

    Add: define 'Place' with properties and structures 👷🏻‍♂️

--- NormandieSpots/Model/Place.swift
+++ NormandieSpots/Model/Place.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreLocation
+
+/// Defines properties and structures for a given 'Place'
+struct Place: Hashable, Codable, Identifiable {
+    /// Main properties
+    var id: Int
+    var name: String
+    var state: String
+    var description: String
+    var isFavorite: Bool
+    
+    /// Image declaration
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    /// Location handler
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    /// Coordinates structure reflecting coordinates stored in placesData.JSON
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
