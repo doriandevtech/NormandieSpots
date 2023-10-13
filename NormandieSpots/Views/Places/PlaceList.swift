32a7578efb1fb138f3667f4535a211ed9c309f61
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-10-13 13:36:40 +0200

    Add: New groups and create PlaceList view 🗂️

---
+++ NormandieSpots/Views/Places/PlaceList.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceList.swift
+//  NormandieSpots
+//
+//  Created by Dorian Emenir on 13/10/2023.
+//
+
+import SwiftUI
+
+struct PlaceList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaceList()
+}
