9ddbc85312f8e02d1bcbadf790f798b7742792b9
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-11-09 19:22:28 +0100

    Create: favorite button and display according to its value 👷🏻‍♂️

--- NormandieSpots/Views/Helpers/FavoriteButton.swift
+++ NormandieSpots/Views/Helpers/FavoriteButton.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
     }
 }
 
-#Preview {
-    FavoriteButton()
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(false))
+    }
 }
