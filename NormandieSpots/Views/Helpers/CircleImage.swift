8cdd5791990f8d90d8d831d138f94cfcd33834a3
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-11-09 19:22:59 +0100

    Create: circle image to design an image for each places 👨🏻‍🎨

--- NormandieSpots/Views/Helpers/CircleImage.swift
+++ NormandieSpots/Views/Helpers/CircleImage.swift
@@ -5,4 +5,23 @@
 //  Created by Dorian Emenir on 09/11/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 5)
+            }
+            .shadow(radius: 5)
+    }
+}
+
+struct CircleImage_Preview: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("etretat"))
+    }
+}
