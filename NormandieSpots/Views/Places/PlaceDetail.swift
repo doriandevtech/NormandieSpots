d584527b547799abb328c0f13377ce787fbb1878
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-11-09 19:21:12 +0100

    Create: detailed view for each places 👷🏻‍♂️

--- NormandieSpots/Views/Places/PlaceDetail.swift
+++ NormandieSpots/Views/Places/PlaceDetail.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct PlaceDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var place: Place
+    
+    var placeIndex: Int {
+        modelData.places.firstIndex(where: { $0.id == place.id })!
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            MapView(coordinate: place.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage(image: place.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(place.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.places[placeIndex].isFavorite)
+                }
+                
+                HStack {
+                    Text(place.category)
+                    Spacer()
+                    Text(place.country)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(place.name)")
+                    .font(.title2)
+                Text(place.description)
+            }
+            .padding()
+        }
+        .navigationTitle(place.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
-#Preview {
-    PlaceDetail()
+struct PlaceDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
+    static var previews: some View {
+        PlaceDetail(place: modelData.places[0])
+            .environmentObject(modelData)
+    }
 }
