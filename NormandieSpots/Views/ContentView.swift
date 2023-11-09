01319e7318c045bdc25d9d1b486d9b2de657b0bf
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-11-09 19:23:38 +0100

    Update: content preview with correct environmentObject 👷🏻‍♂️

--- NormandieSpots/Views/ContentView.swift
+++ NormandieSpots/Views/ContentView.swift
@@ -9,16 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        PlaceList()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
 }
