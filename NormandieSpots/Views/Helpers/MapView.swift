529b88e305b6e304012922ec9ea95b906eb5fd80
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-11-09 19:21:54 +0100

    Create: map view to dusplay a location regarding coordinates 📍

--- NormandieSpots/Views/Helpers/MapView.swift
+++ NormandieSpots/Views/Helpers/MapView.swift
@@ -6,13 +6,30 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    
+    @State private var region = MKCoordinateRegion()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
-#Preview {
-    MapView()
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 48.84149, longitude: 2.26639))
+    }
 }
