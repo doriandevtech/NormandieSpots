9bbb4a2e4253f0a8553c67d41908bb38bdc8fe7f
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-11-09 18:04:54 +0100

    Cretae: row view to showcase a particular place with "isFavorite" attribute 👷🏻‍♂️

---
+++ NormandieSpots/Views/Places/PlaceRow.swift
@@ -0,0 +1,32 @@
+//
+//  PlaceRow.swift
+//  NormandieSpots
+//
+//  Created by Dorian Emenir on 09/11/2023.
+//
+
+import SwiftUI
+
+struct PlaceRow: View {
+    var place: Place
+    
+    var body: some View {
+        HStack {
+            place.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(place.name)
+        }
+    }
+}
+
+struct PlaceRow_Previews: PreviewProvider {
+    static var places = ModelData().places
+    
+    static var previews: some View {
+        Group {
+            PlaceRow(place: places[0])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
