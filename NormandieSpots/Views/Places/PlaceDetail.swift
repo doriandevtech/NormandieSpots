cdc98b9bdaa1babb74a1334053e29ac080612f98
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-11-09 18:17:25 +0100

    Create: Detail view to display details about places 👷🏻‍♂️

---
+++ NormandieSpots/Views/Places/PlaceDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceDetail.swift
+//  NormandieSpots
+//
+//  Created by Dorian Emenir on 09/11/2023.
+//
+
+import SwiftUI
+
+struct PlaceDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaceDetail()
+}
