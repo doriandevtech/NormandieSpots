0af5dffe72a44a9b154830ec866fc7da052e0353
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-11-09 19:24:32 +0100

    Update: add correct environmentObject to App view ✍️

--- NormandieSpots/NormandieSpotsApp.swift
+++ NormandieSpots/NormandieSpotsApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct NormandieSpotsApp: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
