a86847c15536170da6b1604287009c60374e9640
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-11-18 11:58:01 +0100

    Doc: Documentation NormandieSpots App file ✍️

--- NormandieSpots/NormandieSpotsApp.swift
+++ NormandieSpots/NormandieSpotsApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct NormandieSpotsApp: App {
+    /// Creates a model instance and supply it to ContentView
+    /// @State attribute initializes a model object the same way you use it to initialize properties inside a view
     @StateObject private var modelData = ModelData()
     
     var body: some Scene {
