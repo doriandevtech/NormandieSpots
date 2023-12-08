7995ad3b8c7cf0b80fd5efd6fb8a4d43629fcbac
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-12-08 18:14:49 +0100

    Update: category property for some places 👷🏻‍♂️ & create Category folder with 3 files (Home, Row and Item) 🗂️

---
+++ NormandieSpots/Views/Category/CategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItem.swift
+//  NormandieSpots
+//
+//  Created by Dorian Emenir on 08/12/2023.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItem()
+}
---
+++ NormandieSpots/Views/Category/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  NormandieSpots
+//
+//  Created by Dorian Emenir on 08/12/2023.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
