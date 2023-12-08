ca45385806be16b6f6ba1f307b58bb6856ead194
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-12-08 18:16:26 +0100

    Add: Place's image and name in VStack 👷🏻‍♂️

--- NormandieSpots/Views/Category/CategoryItem.swift
+++ NormandieSpots/Views/Category/CategoryItem.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct CategoryItem: View {
+    var place: Place
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            place.image
+                .resizable()
+                .frame(width: 150, height: 150)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(radius: 2)
+            Text(place.name)
+                .foregroundStyle(.primary)
+                .font(.title3)
+        }
     }
 }
 
 #Preview {
-    CategoryItem()
+    CategoryItem(place: ModelData().places[0])
 }
