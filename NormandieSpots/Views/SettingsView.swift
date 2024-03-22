0ef3092cc35d18bb79e01a3c8983e4f8108f3105
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2024-03-22 19:35:52 +0100

    Create: Setting struct and SettingsView to prsonalize the UX in the app 👨🏻‍🎨🗂️

---
+++ NormandieSpots/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  NormandieSpots
+//
+//  Created by Dorian Emenir on 22/03/2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
