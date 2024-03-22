24337070db72a3f3b644c92fd25cb8e9a191997e
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2024-03-22 19:37:11 +0100

    Create: SettingsView to display a view for settings wich contains a basic slider for now with dummy datas 👷🏻‍♂️

--- NormandieSpots/Views/SettingsView.swift
+++ NormandieSpots/Views/SettingsView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Binding var setting: Setting
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        List {
+            Slider(value: $setting.textSizeAsDouble, in: 1...10, step: 1) {
+                Text("Slider")
+//            }
+        }
     }
 }
 
 #Preview {
-    SettingsView()
+    SettingsView(setting: .constant(Setting.sampleData[0]))
 }
