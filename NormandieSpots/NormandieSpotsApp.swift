75e9d575f45e63f74afb9ef8b567b029ad7e8d3b
Dorian <dorianemenir@gmail.com>
2024-03-28 13:08:18 +0100

    Doc: NormandieSpotsApp ✍️

--- NormandieSpots/NormandieSpotsApp.swift
+++ NormandieSpots/NormandieSpotsApp.swift
@@ -5,9 +5,10 @@
 //  Created by Dorian Emenir on 30/09/2023.
 //
 
-import SwiftUI
+// MARK: Imports
+import SwiftUI /// Imports SwiftUI framework
 
-@main
+@main /// @main - defines the entry point of the NormandieSpotsApp
 struct NormandieSpotsApp: App {
     /// Creates a model instance and supply it to ContentView
     /// @State attribute initializes a model object the same way you use it to initialize properties inside a view
