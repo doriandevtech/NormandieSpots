732301e2a6a51193640f65e79a36fe97878239e0
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-04-11 20:31:55 +0200

    Update: Form for new place - add new sections 👷🏻‍♂️ + isAddButtonEnabled state variable 🔗

--- NormandieSpots/Views/Places/NewPlaceView.swift
+++ NormandieSpots/Views/Places/NewPlaceView.swift
@@ -8,16 +8,25 @@
 import SwiftUI
 
 struct NewPlaceView: View {
+    
     @State private var newPlace = Place.emptyPlace
+    
     @Binding var places: [Place]
+    
     @Binding var isPresentingNewPlaceSheet: Bool
+    
+    @State private var isAddButtonEnabled = true
     
     var body: some View {
         NavigationView {
             Form {
-                Section("Nouveau lieu") {
+                Section("Quel est le nom de ce nouveau lieu ?") {
                     TextField("Nom du lieu", text: $newPlace.name)
+                }
+                Section("Où se situe ce lieu ?") {
                     TextField("Nom du pays", text: $newPlace.country)
+                }
+                Section("Ajouter ce lieu aux favoris ?") {
                     Toggle(isOn: $newPlace.isFavorite) {
                         Label("Ajouter aux favoris", systemImage: "star.fill")
                     }
@@ -25,15 +34,16 @@ struct NewPlaceView: View {
             }
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
-                    Button("Dismiss") {
+                    Button("Annuler") {
                         isPresentingNewPlaceSheet = false
                     }
                 }
                 ToolbarItem(placement: .confirmationAction) {
-                    Button("Add") {
+                    Button("Ajouter") {
                         places.append(newPlace)
                         isPresentingNewPlaceSheet = false
                     }
+                    .disabled(isAddButtonEnabled)
                 }
             }
         }
