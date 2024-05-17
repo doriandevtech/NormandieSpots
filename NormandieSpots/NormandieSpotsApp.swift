2571ded4699960999cdccd5c2bf1ab8a5b12d593
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-05-17 15:13:58 +0200

    Update: load data on app launch 💾

--- NormandieSpots/NormandieSpotsApp.swift
+++ NormandieSpots/NormandieSpotsApp.swift
@@ -13,12 +13,19 @@ struct NormandieSpotsApp: App {
     /// Creates a model instance and supply it to ContentView
     /// @State attribute initializes a model object the same way you use it to initialize properties inside a view
     @StateObject private var modelData = ModelData()
-    @State private var places = Place.sampleData
+    @StateObject private var store = PlaceStore()
         
     var body: some Scene {
         WindowGroup {
-            ContentView(places: $places)
+            ContentView(places: $store.places)
                 .environmentObject(modelData)
+                .task {
+                    do {
+                        try await store.load()
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
         }
     }
 }
